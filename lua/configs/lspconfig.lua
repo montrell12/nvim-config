ef7bbdaa621597bdd77b77ef8839c571787533fc
Montrell Pegue <montrelldpegues@gmail.com>
2025-12-15 20:29:44 +0000

    Initial commit

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,37 @@
 require("nvchad.configs.lspconfig").defaults()
+local lspconfig = require "lspconfig"
 
 local servers = { "html", "cssls" }
 vim.lsp.enable(servers)
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
 
--- read :h vim.lsp.config for changing options of lsp servers 
+lspconfig.ts_ls.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+}
+
+
+
+lspconfig.tailwindcss.setup({
+  root_dir = lspconfig.util.root_pattern("tailwind.config.js", "tailwind.config.ts", "postcss.config.js"),
+  settings = {
+    tailwindCSS = {
+      includeLanguages = {
+        typescript = "javascript",
+        typescriptreact = "javascriptreact",
+        javascript = "javascript",
+        javascriptreact = "javascriptreact",
+      },
+      experimental = {
+        classRegex = {
+          "tw`([^`]*)",
+          'tw="([^"]*)"',
+          'tw={"([^"}]*)"}',
+        },
+      },
+    },
+  },
+})
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,14 @@
 require "nvchad.mappings"
 
--- add yours here
+
 
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map('t', 'jk', [[<C-\><C-n>]], opts)
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map('v', '<A-j>', ":m '>+1<CR>gv=gv", opts)
+map('v', '<A-k>', ":m '<-2<CR>gv=gv", opts)
+
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
